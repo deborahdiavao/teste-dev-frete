71aa6da7b9f8bf0fd4b7e7cf117ba6c3df0ca3fb
Déborah Diavão <deborahdiavao@gmail.com>
2026-03-02 12:30:28 -0300

    Add Frete class for freight calculation

--- app/models/frete.py
+++ app/models/frete.py
@@ -1,1 +1,12 @@
 
+class Frete:
+    def __init__(self, peso: float, distancia: float):
+        self.peso = peso
+        self.distancia = distancia
+
+    def calcular(self) -> float:
+        valor_base = 5.0
+        valor_por_kg = 2.0
+        valor_por_km = 0.5
+
+        return valor_base + (self.peso * valor_por_kg) + (self.distancia * valor_por_km)
